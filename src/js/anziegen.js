1e28c583ce5a566a69bc8f72fd850c603791cb3e
emxrxan <emirgurk@gmail.com>
2023-06-13 13:37:03 +0200

    Create

---
+++ src/js/anziegen.js
@@ -0,0 +1,23 @@
+"use-strict";
+
+const show_project = function(){
+    document.querySelectorAll(".dropdown_project").forEach(element =>{
+        element.classList.toggle("show_dropdown_project");
+    });
+}
+
+const show_dropdown = function(){
+    document.querySelector(".navigation_dropdown").classList.toggle("show_navigation_dropdown")
+}
+
+const set_darkmode = function(){
+    document.querySelector("body").classList.toggle("darkmode");
+    const button = document.querySelector(".darkmode-container > #button");
+    if (button.querySelector(".material-symbols-outlined").innerText == "dark_mode"){
+        button.querySelector(".material-symbols-outlined").innerText = "light_mode";
+        button.style.left = "48px";
+    } else {
+        button.querySelector(".material-symbols-outlined").innerText = "dark_mode";
+        button.style.left = "0px";
+    }
+}
